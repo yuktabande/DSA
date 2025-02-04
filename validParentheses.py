abd66a76bbef1ca57079815af2abaf0af8612939
Yukta Bande <yuktaabande@gmail.com>
2025-02-04 21:40:02 +0530

    valid Parentheses

---
+++ validParentheses.py
@@ -0,0 +1,15 @@
+s = "()"
+
+# stack.append(1)
+# stack.append(2)
+# print(stack.pop()) #2
+# print(stack)#1
+stack = []
+pairs = {")": "(","}": "{", "]":"["}
+for char in s:
+    if char in pairs.values():
+        stack.append(char)
+    elif char in pairs:
+        if not stack or stack.pop() != pairs[char]:
+            return False
+return not stack

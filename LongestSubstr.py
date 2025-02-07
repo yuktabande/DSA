3fda5e4e7234b953940760712bbd03007ad45830
Yukta Bande <yuktaabande@gmail.com>
2025-02-07 23:25:28 +0530

    longest substring

---
+++ LongestSubstr.py
@@ -0,0 +1,10 @@
+s = "pwwkew"
+output = ""
+for char in s: #k
+    if char not in output: #k
+        output += char #output = pwk
+        
+
+    if output not in s:
+        output = char #output = 
+print(output)

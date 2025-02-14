cc312d5cf10d5a5d7012be52e7977a7aa3c0a78c
Yukta Bande <yuktaabande@gmail.com>
2025-02-14 22:57:10 +0530

    commented process

---
+++ wordSearch.py
@@ -0,0 +1,9 @@
+#given an array; to check if the string exists, vertically or horizontally adj
+#while letter in string
+#for i in range 
+#   for j in range 
+#       if array[i][j] == string's 1st letter:
+#           for letter in string in range 1,n:
+#               if letter is not array[i+1][j] or array[i][j+1]:
+#                   return false
+#           return true

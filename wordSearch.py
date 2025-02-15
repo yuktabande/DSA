769048ef15c66811fb272a4cc98ad086ea3da822
Yukta Bande <yuktaabande@gmail.com>
2025-02-15 22:41:35 +0530

    changes

--- wordSearch.py
+++ wordSearch.py
@@ -1,5 +1,4 @@
 #given an array; to check if the string exists, vertically or horizontally adj
-#while letter in string
 #for i in range 
 #   for j in range 
 #       if array[i][j] == string's 1st letter:
@@ -7,3 +6,17 @@
 #               if letter is not array[i+1][j] or array[i][j+1]:
 #                   return false
 #           return true
+
+board = [["A", "B", "C", "E"],
+        ["S", "F", "C", "S"],
+        ["A", "D", "E", "E"]]
+
+word = "ABCCED"
+total_elements = sum(len(row) for row in board)
+for i in range(0, len(board[0]+1)):
+    for j in range(i, len(board[0]+1)):
+        if board[i][j] == word[0]:
+            for letter in word:
+                if letter != board[i+1][j] or letter != board[i][j+1]:
+                    print("False")
+            print("True")

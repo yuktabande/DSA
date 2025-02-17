d841a2616fc609c593fc8b56d2dc2690b789a478
Yukta Bande <yuktaabande@gmail.com>
2025-02-17 19:12:48 +0530

    Armstrong number

---
+++ armstrong.py
@@ -0,0 +1,10 @@
+num = 153
+last_digit = num%10
+str_num = str(num)
+total = 0
+for digit in str_num:
+    total += int(digit)**last_digit
+if total == num:
+    print("Armstrong")
+else:
+    print("Not Armstrong")

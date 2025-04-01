3b1d6507beffc96036d0c9beaa5a69eecd7982af
Yukta Bande <yuktaabande@gmail.com>
2025-04-01 20:43:45 +0530

    selection sort

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -2,3 +2,31 @@
 first the entire array is unsorted. Select the smallest element and 
 swap it with 0th position element. Now in unsorted list, find the smallest element
 and add it to the sorted list*/
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[], int n){
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout << endl;
+} 
+void selectionSort(int arr[], int n) {
+    for(int i=0; i<n-1; i++){
+    int smallestIdx = i; //unsorted part starting
+    for(int j=i+1; j<n; j++){
+        if(arr[j] < arr[smallestIdx]) {
+            smallestIdx = j;
+        }
+    }
+    swap(arr[i], arr[smallestIdx]);
+}
+}
+
+int main(){
+    int n = 5;
+    int arr[] = {4,1,5,2,3};
+    selectionSort(arr,n);
+    printArray(arr, n);
+    return 0; 
+}

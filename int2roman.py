7c510d9db0c231edc5e7e2c8162809fb1bb574f0
Yukta Bande <159476987+yuktabande@users.noreply.github.com>
2025-05-19 10:07:05 +0530

    Add files via upload

---
+++ int2roman.py
@@ -0,0 +1,11 @@
+num = 3749
+values = [
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"), (10, "X"),
+            (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+result = []
+for value, symbol in values:
+    while num >= value:
+        result.append(symbol)
+        num -= value
+print(''.join(result))

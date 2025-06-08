1ff29eefa59aeb04e1701db34018a3507ce772f9
Yukta Bande <159476987+yuktabande@users.noreply.github.com>
2025-06-08 09:07:36 +0530

    Create groceryCost.py

---
+++ groceryCost.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+while t > 0:
+    n, x = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    total_cost = 0
+    # Your code goes here
+    for i in range(n):
+        if a[i] >= x:
+            total_cost += b[i]
+            
+    print(total_cost)
+    t -= 1

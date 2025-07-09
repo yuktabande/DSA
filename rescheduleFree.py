25f37d9ac8d8d35cbe4393e6f62cc3dc949da9de
Yukta Bande <159476987+yuktabande@users.noreply.github.com>
2025-07-09 11:31:06 +0530

    Add files via upload

---
+++ rescheduleFree.py
@@ -0,0 +1,15 @@
+eventTime = 5
+k = 1
+startTime = [1,3]
+endTime = [2,5]
+
+n = len(startTime)
+res = 0
+total = [0] * (n+1)
+for i in range(n):
+    total[i + 1] = total[i] + endTime[i] - startTime[i]
+for i in range(k - 1,n):
+    right = eventTime if i==n - 1 else startTime[i + 1]
+    left = 0 if i == k - 1 else endTime[i - k]
+    res = max(res, right - left - (total[i+1] - total[i-k+1]))
+print(res)
